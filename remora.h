9dad5a197cbc2fca02811f55b2c62fbee67a2dab
Cr4shover1de <80235413+Cr4shover1de@users.noreply.github.com>
2025-06-09 13:41:38 -0500

    Update remora.h

--- remora.h
+++ remora.h
@@ -1,11 +1,4 @@
-#ifndef REMORA_H
-#define REMORA_H
+#pragma once
 
-#include "mbed.h"
-
-// Definitions and prototypes for SPI transport and Remora
-
-void remora_init();
-void remora_poll();
-
-#endif // REMORA_H
+void remoraInit();
+void remoraMain();
