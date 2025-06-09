bcacb8636c449e0f564005088cf9557e82490145
Cr4shover1de <80235413+Cr4shover1de@users.noreply.github.com>
2025-06-09 02:32:18 -0500

    Create remora.h

---
+++ Remora-OS5/remora.h
@@ -0,0 +1,11 @@
+#ifndef REMORA_H
+#define REMORA_H
+#include "mbed.h"
+
+class RemoraApp {
+public:
+    void setup() { /* SPI, encoder, DAC setup */ }
+    void loop() { /* main firmware loop */ }
+};
+
+#endif
