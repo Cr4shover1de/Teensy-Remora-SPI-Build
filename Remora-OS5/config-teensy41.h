6863a974beb1176b1e217bb65a79e71021d98c5d
Cr4shover1de <80235413+Cr4shover1de@users.noreply.github.com>
2025-06-09 02:35:53 -0500

    Create config-teensy41.h

---
+++ Remora-OS5/config-teensy41.h
@@ -0,0 +1,16 @@
+#define SPI_CHANNEL     1
+#define SPI_CS_PIN      10
+
+#define STEP1_PIN       2
+#define DIR1_PIN        3
+#define STEP2_PIN       4
+#define DIR2_PIN        5
+
+#define ENC1_CHANNEL    1
+#define ENC1_A_PIN      6
+#define ENC1_B_PIN      7
+#define ENC1_Z_PIN      8
+
+#define DAC0_PIN        A21
+
+#endif
